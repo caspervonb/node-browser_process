10d148be793c61dfe8492545e5e48ef53d9e5cac
Casper Beyer <caspervonb@qup.io>
2015-08-04 17:05:11 +0800

    Revert "Remove browser type lookup in options"
    This reverts commit 471ec45cb500a4aaffbc8a9f1bbe3698c348a2af.
    
    It should not have gone into a patch release and should be
    considered a bug since it breaks code without warning.

--- test/lib_options.js
+++ test/lib_options.js
@@ -19,17 +19,31 @@ var types = {
   ],
 };
 
-Object.keys(types).forEach(function (type) {
-  test(type + ' options', function (assert) {
-    assert.plan(1);
-    var args = browser.options(type, {
-      private: true,
-      window: true,
-      profile: 'profile_dir',
-      debug: 4000,
-      url: 'about:blank',
-    });
+var names = {
+  'chrome': 'chrome',
+  'chromium': 'chrome',
+  'electron': 'chrome',
+  'firefox': 'firefox',
+};
 
-    assert.deepEqual(args, types[type]);
+Object.keys(names).forEach(function (name) {
+  var type = names[name];
+
+  test(name + ' options', function (assert) {
+    assert.plan(2);
+
+    browser.find(name, function (error, command) {
+      assert.error(error);
+
+      var args = browser.options(command, {
+        private: true,
+        window: true,
+        profile: 'profile_dir',
+        debug: 4000,
+        url: 'about:blank',
+      });
+
+      assert.deepEqual(args, types[type]);
+    });
   });
 });
