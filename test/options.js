b0bb136efc6d2237bcc59af9b95d53fa8f7951c4
Casper Beyer <caspervonb@qup.io>
2015-12-09 17:13:39 +0800

    test: seperate different browser type tests
    When testing the actual expected options or spawn behavior, chrome,
    firefox and electron have little in common. So we end up having branches
    trying to accomodate them all in the same test.
    
    This splits them out into multiple tests and uses `skip` on tape to skip
    the test when it is not relevant.

--- test/options.js
+++ test/options.js
@@ -4,26 +4,46 @@ const browser = require('..');
 const test = require('tape');
 
 const name = (process.env['TEST_BROWSER'] || 'chrome');
+const type = browser.type(name);
 
-const options = {
-  chrome: {
+test(`chrome options`, { skip: !type.match(/chrome/) }, assert => {
+  const options = {
     debug: { value: 4000, expect: ['--remote-debugging-port=4000'] },
     private: { value: true, expect: ['--incognito'] },
     url: { value: 'about:blank', expect: ['about:blank'] },
     window: { value: true, expect: ['--new-window'] },
-  },
+  };
 
-  firefox: {
+  const keys = Object.keys(options);
+
+  assert.plan(keys.length * 3);
+
+  keys.forEach(key => {
+    let option = options[key];
+
+    assert.deepEqual(browser.options(name, {
+      [key]: option.value,
+    }), option.expect);
+
+    browser.find(name, (error, command) => {
+      assert.error(error);
+      assert.deepEqual(browser.options(command, {
+        [key]: option.value,
+      }), option.expect);
+    });
+  });
+});
+
+test(`firefox options`, { skip: !type.match(/firefox/) }, assert => {
+  const options = {
     debug: { value: 4000, expect: ['--start-debugging-server', '4000'] },
     private: { value: true, expect: ['--private'] },
     url: { value: 'about:blank', expect: ['about:blank'] },
     window: { value: true, expect: ['--new-window'] },
-  },
-}[browser.type(name)];
+  };
 
-const keys = Object.keys(options);
+  const keys = Object.keys(options);
 
-test(`options for ${name}`, assert => {
   assert.plan(keys.length * 3);
 
   keys.forEach(key => {
--- test/spawn.js
+++ test/spawn.js
@@ -2,23 +2,50 @@
 
 const browser = require('..');
 const test = require('tape');
-const path = require('path');
+const http = require('http');
 
 const name = (process.env['TEST_BROWSER'] || 'chrome');
 
-test(`spawn ${name}`, assert => {
+test(`spawn ${name}`, { skip: name.match(/electron/) }, assert => {
   assert.plan(3);
 
-  browser.spawn(name, (error, ps) => {
+  let server = http.createServer();
+  server.once('listening', () => {
+    let address = server.address();
+    browser.spawn(name, [`http://localhost:${address.port}`], (error, ps) => {
+      assert.error(error);
+      assert.ok(ps);
+
+      server.once('request', () => {
+        ps.once('close', () => {
+          server.once('close', () => {
+            assert.pass('close');
+          });
+
+          server.close();
+        });
+
+        ps.kill();
+      });
+    });
+  });
+
+  server.listen();
+});
+
+test(`spawn electron`, { skip: name.match(/electron/) }, assert => {
+  assert.plan(3);
+
+  browser.spawn(name, ['--version'], (error, ps) => {
     assert.error(error);
     assert.ok(ps);
 
-    setTimeout(() => {
-      ps.once('close', (code) => {
+    ps.stdout.once('data', () => {
+      ps.once('close', () => {
         assert.pass('close');
       });
 
       ps.kill();
-    }, 1000);
+    });
   });
 });
