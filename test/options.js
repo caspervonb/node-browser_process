c9fb4cc805b954de332181a32e3525ba2f27bcdd
Casper Beyer <caspervonb@qup.io>
2015-12-15 21:57:42 +0800

    test: add electron options test
    With electron being its own type, and chrome having contant defaults the
    options test needs a bit of rewriting.

--- test/options.js
+++ test/options.js
@@ -4,14 +4,53 @@ const browser = require('..');
 const test = require('tape');
 
 const name = (process.env['TEST_BROWSER'] || 'chrome');
-const type = browser.type(name);
 
-test(`chrome options`, { skip: !type.match(/chrome/) }, assert => {
+test(`chrome options`, { skip: !name.match(/chrome|chromium/) }, assert => {
   const options = {
-    debug: { value: 4000, expect: ['--remote-debugging-port=4000'] },
-    private: { value: true, expect: ['--incognito'] },
-    url: { value: 'about:blank', expect: ['about:blank'] },
-    window: { value: true, expect: ['--new-window'] },
+    debug: {
+      value: 4000,
+      expect: [
+        '--remote-debugging-port=4000',
+        '--no-default-browser-check',
+        '--no-first-run',
+      ],
+    },
+
+    private: {
+      value: true,
+      expect: [
+        '--incognito',
+        '--no-default-browser-check',
+        '--no-first-run',
+      ],
+    },
+
+    profile: {
+      value: '/profile/path',
+      expect: [
+        '--user-data-dir=/profile/path',
+        '--no-default-browser-check',
+        '--no-first-run',
+      ],
+    },
+
+    url: {
+      value: 'http://localhost:4000',
+      expect: [
+        'http://localhost:4000',
+        '--no-default-browser-check',
+        '--no-first-run',
+      ],
+    },
+
+    window: {
+      value: true,
+      expect: [
+        '--new-window',
+        '--no-default-browser-check',
+        '--no-first-run',
+      ],
+    },
   };
 
   const keys = Object.keys(options);
@@ -27,6 +66,7 @@ test(`chrome options`, { skip: !type.match(/chrome/) }, assert => {
 
     browser.find(name, (error, command) => {
       assert.error(error);
+
       assert.deepEqual(browser.options(command, {
         [key]: option.value,
       }), option.expect);
@@ -34,12 +74,21 @@ test(`chrome options`, { skip: !type.match(/chrome/) }, assert => {
   });
 });
 
-test(`firefox options`, { skip: !type.match(/firefox/) }, assert => {
+test(`electron options`, { skip: !name.match(/electron/) }, assert => {
   const options = {
-    debug: { value: 4000, expect: ['--start-debugging-server', '4000'] },
-    private: { value: true, expect: ['--private'] },
-    url: { value: 'about:blank', expect: ['about:blank'] },
-    window: { value: true, expect: ['--new-window'] },
+    debug: {
+      value: 4000,
+      expect: [
+        '--remote-debugging-port=4000',
+      ],
+    },
+
+    url: {
+      value: 'index.js',
+      expect: [
+        'index.js',
+      ],
+    },
   };
 
   const keys = Object.keys(options);
@@ -55,6 +104,68 @@ test(`firefox options`, { skip: !type.match(/firefox/) }, assert => {
 
     browser.find(name, (error, command) => {
       assert.error(error);
+
+      assert.deepEqual(browser.options(command, {
+        [key]: option.value,
+      }), option.expect);
+    });
+  });
+});
+
+test(`chrome options`, { skip: !name.match(/firefox/) }, assert => {
+  const options = {
+    debug: {
+      value: 4000,
+      expect: [
+        '--start-debugging-server',
+        4000,
+      ],
+    },
+
+    private: {
+      value: true,
+      expect: [
+        '--private',
+      ],
+    },
+
+    profile: {
+      value: '/profile/path',
+      expect: [
+        '--profile',
+        '/profile/path'
+      ],
+    },
+
+    url: {
+      value: 'http://localhost:4000',
+      expect: [
+        'http://localhost:4000',
+      ],
+    },
+
+    window: {
+      value: true,
+      expect: [
+        '--new-window',
+      ],
+    },
+  };
+
+  const keys = Object.keys(options);
+
+  assert.plan(keys.length * 3);
+
+  keys.forEach(key => {
+    let option = options[key];
+
+    assert.deepEqual(browser.options(name, {
+      [key]: option.value,
+    }), option.expect);
+
+    browser.find(name, (error, command) => {
+      assert.error(error);
+
       assert.deepEqual(browser.options(command, {
         [key]: option.value,
       }), option.expect);
