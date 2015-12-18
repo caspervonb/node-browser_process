b57a09ce5bfbe3c4d4fc8f4b727e066c3809993a
Casper Beyer <caspervonb@qup.io>
2015-12-19 05:29:16 +0800

    lib: add synchronous version of `detect`
    Its sometimes useful to detect a browser synchroniously, for example as
    a default in a command line tool. This adds a synchronous version of
    `detect` called `detectSync`

--- test/detect.js
+++ test/detect.js
@@ -7,7 +7,14 @@ const path = require('path');
 test(`detect browsers`, assert => {
   assert.plan(1);
 
-  browser.detect((results) => {
+  browser.detect(results => {
     assert.ok(results);
   });
+});
+
+test(`detect browsers synchronously`, assert => {
+  assert.plan(1);
+
+  let results = browser.detectSync();
+  assert.ok(results);
 });
